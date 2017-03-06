e9cf85104d39ef99eb5d6ae9becd048da8dc141f
designdnt <thomas.doubek@gmail.com>
2017-03-06 11:39:35 +0100

    bash rewrited code for creat and del vendor

--- dnt-bash/newvendor.php
+++ dnt-bash/newvendor.php
@@ -4,25 +4,9 @@ $autoload		= new Autoload;
 $path			= "../";
 $autoload->load($path);
 
-//instance
-$db 			= new Db;
-$XMLgenerator	= new XMLgenerator;
-$api			= new Api;
-
-//settings
-$COPY_FROM 		= 2;
-$NEW_VENDOR_ID 	= 3;
-$STATUS 		= "creat"; //add;dell
-
-//tabulky
+$VENDOR_NAME 	= "Pdc";
+$COPY_FROM 		= "3";
 $tables = array(
-	//"dnt_api",
-	//"dnt_languages",
-	/*"dnt_posts",
-	"dnt_post_type",
-	"dnt_polls",
-	"dnt_polls_composer",
-	*/
 	//VSETKY STLPCE
 	"dnt_admin_menu",
 	"dnt_api",
@@ -41,7 +25,6 @@ $tables = array(
 	"dnt_uploads",
 	"dnt_users",
 );
-
 
 function getColumns($query){
 		$db = new DB();
@@ -64,12 +47,24 @@ function getColumns($query){
 		}
 	}
 	
-if($STATUS == "del"){
-	foreach($tables as $table){
-		$where = array( 'vendor_id' => $NEW_VENDOR_ID);
-		$db->delete( $table, $where);
-	}
-}else{
+function addVendor($tables, $VENDOR_NAME, $COPY_FROM){
+	$db 			= new Db;
+	//SETTINGS
+	$COPY_FROM 			= $COPY_FROM;
+	$vendor_name		= $VENDOR_NAME;
+	$vendor_name_url	= Dnt::name_url($VENDOR_NAME);
+	$vendor_layout		= Dnt::name_url($VENDOR_NAME);
+	
+	$insertedData = array(
+		'name' 			=> $vendor_name, 
+		'name_url' 		=> $vendor_name_url, 
+		'layout' 		=> $vendor_layout, 
+		'is_default' 	=> 0, 
+
+	);
+	$db->insert('dnt_vendors', $insertedData);
+	$NEW_VENDOR_ID = $db->lastid();
+	
 	foreach($tables as $table){
 		//tabulka
 		$query = "SELECT * FROM $table WHERE vendor_id = '$COPY_FROM'";
@@ -109,35 +104,36 @@ if($STATUS == "del"){
 		$db->delete( $table, $where);
 	}
 	
-	//CREAT POST COMPOSER
+	//INSERT POST_TYPE FROM
+	$query =
+	"
+	INSERT INTO `dnt_post_type` (`id`, `cat_id`, `sub_cat_id`, `name_url`, `admin_cat`, `name`, `show`, `order`, `vendor_id`, `parent_id`) VALUES 
+	(NULL, '1', '0', 'sitemap', 'sitemap', 'Stránky', '1', '0', '$NEW_VENDOR_ID ', '0'),
+	(NULL, '1', '0', 'sitemap-sub', 'sitemap', 'Podstránky', '1', '0', '$NEW_VENDOR_ID ', '0'),
+	(NULL, '1', '0', 'article', 'sitemap', 'Články', '1', '0', '$NEW_VENDOR_ID ', '0'),
+	(NULL, '3', '0', 'newsletter', 'post', 'Newslettre', '1', '0', '$NEW_VENDOR_ID ', '0'),
+	(NULL, '2', '0', 'domace', 'article', 'Domáce', '1', '0', '$NEW_VENDOR_ID ', '0')
+	";
+	$db->query($query);
+
 }
 
+function delVendor($id, $tables){
+	
+	$db = new Db;
 
-//var_dump($api->getColumns($query));
-/*
-foreach($api->getColumns($query) as $column){
-	echo $column;
-}
-*/
-
-/*
-if ($db->num_rows($query) > 0){
-	foreach($db->get_results($query) as $row){
-		return $row[$column];
+	foreach($tables as $table){
+		$where = array( 'vendor_id' => $id);
+		$db->delete( $table, $where);
 	}
-}else{
-	return false;
+	$query =
+	"
+	DELETE FROM `dnt_vendors` WHERE `dnt_vendors`.`id` = '$id';
+	";
+	$db->query($query);
+	
+	echo "Deleted";
 }
-*/
 
-/*
-//SQL POST_TYPE
-INSERT INTO `dnt_post_type` (`id`, `cat_id`, `sub_cat_id`, `name_url`, `admin_cat`, `name`, `show`, `order`, `vendor_id`, `parent_id`) VALUES 
-(NULL, '1', '0', 'sitemap', 'sitemap', 'Stránky', '1', '0', '2', '0'),
-(NULL, '1', '0', 'sitemap-sub', 'sitemap', 'Podstránky', '1', '0', '2', '0'),
-(NULL, '1', '0', 'article', 'sitemap', 'Články', '1', '0', '2', '0'),
-(NULL, '3', '0', 'newsletter', 'post', 'Newslettre', '1', '0', '2', '0'),
-(NULL, '2', '0', 'domace', 'article', 'Domáce', '1', '0', '2', '0')
-*/
-		
-echo "OK";
+addVendor($tables, $VENDOR_NAME, $COPY_FROM);
+//delVendor(7 , $tables);
