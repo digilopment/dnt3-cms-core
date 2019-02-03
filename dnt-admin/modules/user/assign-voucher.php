1f1364c4bcd9646d1614f1c48d59cd84ea54e32c
designdnt <thomas.doubek@gmail.com>
2019-02-03 20:28:12 +0100

    error message no vouchers

--- dnt-admin/modules/user/assign-voucher.php
+++ dnt-admin/modules/user/assign-voucher.php
@@ -4,5 +4,10 @@ if(Voucher::assignLastVoucher($post_id)){
 	$url = "index.php?src=".$rest->get("src")."&type=".$rest->get("type")."";
 	Dnt::redirect($url);
 }else{
-	echo "nie je mozne pridat voucher";
+	include "tpl_functions.php";
+	get_top();
+	include "top.php";
+	error_message_default("Nie je možné pridať voucher. <br/>Pre pridanie vouchera, potrebujete mať naimportovaný zoznam voucherov.");
+	include "bottom.php";
+	get_bottom();
 }
