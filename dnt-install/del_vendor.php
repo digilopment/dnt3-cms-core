3c0e068512c09e7d22d2a28eea75a750471b22d4
designdnt <thomas.doubek@gmail.com>
2019-02-10 00:23:56 +0100

    image dipendency between all tables and physical files in a directory

--- dnt-install/del_vendor.php
+++ dnt-install/del_vendor.php
@@ -5,7 +5,7 @@ $path			= "../";
 $autoload->load($path);
 $install = new Install;
 
-$vendor_id 	= "49";
+$vendor_id 	= "50";
 $tables = array(
 	//VSETKY STLPCE
 	"dnt_admin_menu",
--- dnt-library/framework/_Class/Image.php
+++ dnt-library/framework/_Class/Image.php
@@ -100,4 +100,101 @@ Class Image {
         return self::getFileImage($imageId);
     }
 
+    /**
+     * 
+     * @param type $id
+     * @param type $table
+     * @return type
+     */
+    public function getColumnByName($file) {
+        $db = new Db;
+
+        $query = "SELECT * FROM dnt_uploads WHERE name = '" . $file . "' LIMIT 1";
+        if ($db->num_rows($query) > 0) {
+            foreach ($db->get_results($query) as $row) {
+                return $row['id_entity'];
+            }
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 
+     * @param type $file
+     * @param type $onDelete - pri mazani z vendora je hodnota nastavena na true, alebo nie je nastaveny param
+     * @return string
+     * funkcia vrati true, ak je obrazok pouzity v niektorej tablukle (ak nie je dypendencia, moze sa fyzicky vymazat)
+     * v opacnom pripade sa vrati false
+     * 
+     */
+    public function hasDipendency($file, $onDelete = true) {
+        $db = new Db();
+
+        if (!is_numeric($file)) {
+            $image_id_entity = self::getColumnByName($file);
+        } else {
+            $image_id_entity = $file;
+        }
+        
+        if($onDelete == true){
+            $defaultUploadDipendency = 1;
+        }else{
+            $defaultUploadDipendency = 0;
+        }
+        $data = array();
+        
+        /**
+         * If $image_id_entity
+         */
+        if ($image_id_entity) {
+            
+            /**
+             * skontroluje, ci sa nachadza v dnt_uploads prave 0, alebo 1 zaznam. 
+             * ak ich je viac, ako 1zaznam, web mohol byt skopirovany a subor caka na pouzitie v druhom webe.
+             */
+            $onDelete = $onDelete + 1 - 1; //to integer
+            if ($onDelete != 3) {
+                $query = "SELECT * FROM dnt_uploads WHERE id_entity = '" . $image_id_entity . "'";
+                if ($db->num_rows($query) > $defaultUploadDipendency) {
+                    foreach ($db->get_results($query) as $row) {
+                        $data[] = array(
+                            "image"      => $image_id_entity,
+                            "image_url"  => $row['name'],
+                            "table"      => "dnt_uploads",
+                            "column"     => "id_entity",
+                            "vendor_id"  => $row['vendor_id'],
+                        );
+                    }
+                }
+            }
+            
+            $tables = array(
+                "dnt_polls"             => "img",
+                "dnt_polls_composer"    => "img",
+                "dnt_posts"             => "img",
+                "dnt_posts_meta"        => "value", //value
+                "dnt_registred_users"   => "img",
+                "dnt_settings"          => "value", //value
+                "dnt_users"             => "img", //value
+            );
+
+            foreach ($tables as $table => $column) {
+                $query = "SELECT * FROM $table WHERE `$column` = '" . $image_id_entity . "'";
+                if ($db->num_rows($query) > 0) {
+                    foreach ($db->get_results($query) as $row) {
+                        $data[] = array(
+                            "image"     => $image_id_entity,
+                            "image_url" => $file,
+                            "table"     => $table,
+                            "column"    => $column,
+                            "vendor_id" => $row['vendor_id'],
+                        );
+                    }
+                }
+            }
+        }
+        return $data;
+    }
+
 }
