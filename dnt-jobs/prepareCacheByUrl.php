a20adc49ce2264c060acf27c2850e7463e4ed6b0
designdnt <thomas.doubek@gmail.com>
2019-03-29 21:52:47 +0100

    form Bona test. create URL job first level

--- dnt-jobs/prepareCacheByUrl.php
+++ dnt-jobs/prepareCacheByUrl.php
@@ -7,18 +7,28 @@ $path			= "../";
 $autoload->load($path);
 $vendor = new Vendor;
 
+/*
+$hooks = Webhook::getSitemapModules(false, Vendor::getId());
+foreach($hooks as $hook){
+	var_dump($GLOBALS['DB_PROTOCOL'].$GLOBALS['DB_DOMAIN']."/".$hook);
+}
 
+var_dump($GLOBALS['DB_PROTOCOL'].$GLOBALS['DB_DOMAIN']);
 
+exit;
+*/
 foreach($vendor->getAll() as $vendor){
 	$defaultModule = Webhook::getSitemapModules(false, $vendor['id']);
 	foreach($defaultModule as $module){
 		
 		$defaultUrl = HTTP_PROTOCOL.$vendor['name_url'].".".DOMAIN.WWW_FOLDERS."/".$module;
 		if($vendor['show_real_url'] == 1){
-			$realUrl 	= WWW_PATH.$module;
+			$realUrl 	= $vendor['real_url'].$module;
 			file_get_contents($realUrl);
-		}
+			var_dump($realUrl);
+		}else{
 		file_get_contents($defaultUrl);
 		var_dump($defaultUrl);
+		}
 	}
 }
---
+++ dnt-test/sentBona.php
@@ -0,0 +1,37 @@
+<?php
+//include "autoload.php";
+include "../globals.php";
+include "../dnt-library/framework/_Class/Autoload.php";
+$autoload		= new Autoload;
+$path			= "../";
+$autoload->load($path);
+$dntMailer = new Mailer;
+
+$senderEmail	= "no-reply@markiza.sk";
+$recipientEmail	= "doubek.tomas@markiza.sk";
+$messageTitle	= "Formulár Bona"; 
+
+$msg			= "<html><head></head><body>
+		
+		<h3>Kontaktné údaje</h3>
+		<b>Formulár:</b>: Formulár Bona<br/>
+		<b>Meno</b>: Tatiana <br/>
+		<b>Priezvisko</b>: Opátová<br/>
+		<b>Email</b>: tatianaopatova@gmail.com<br/>
+		<b>Tel.č</b>: 0908132726<br/>
+		<b>Správa</b>:       MŠ-Veľké Bedzany - sme malá jednotriedka, umiestnená v starej budove, kde už všetko končí svoju životnosť.Linoleum v MŠ je v dosť zlom stave.Deti sa niekde naň kopcú, teda nielen po tej estetickej stránke to vyzerá zle, ale aj z hladiska bezpečnosti. Máme deti od 2,5r, ktoré si nevedia dávat taký pozor pri chôdzi či občas aj behu....Aj my by sme si našu materskú školu rady skultúrnili, zútulnili a urobili ju bezpečnejšou pre naších drobcov. 
+S pozdravom
+                                          Tatiana Opátová, riaditeľka MŠ<br/>
+		<br/>
+		<a href='http://storage.services.markiza.sk/dnt-markiza/dnt-system/data/25/uploads/45_e972e8e180eef620f5662a83ae4a0f7a_o.jpg' >45_e972e8e180eef620f5662a83ae4a0f7a_o.jpg</a>
+		</body>
+	</html>";
+$dntMailer->set_recipient(array($recipientEmail));
+$dntMailer->set_msg($msg);
+$dntMailer->set_subject($messageTitle);
+$dntMailer->set_sender_name($senderEmail);
+$dntMailer->set_sender_email($senderEmail);
+$dntMailer->sent_email();
+
+echo rand(0, 999);
+			
