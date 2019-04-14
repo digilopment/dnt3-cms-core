5f37ea8025591b1e17db7e320bbabba286a88905
designdnt <thomas.doubek@gmail.com>
2019-04-14 11:38:10 +0200

    fix productions bug

--- dnt-test/delVendorCache.php
+++ dnt-test/delVendorCache.php
@@ -5,14 +5,10 @@ include "../globals.php";
 $autoload		= new Autoload;
 $path			= "../";
 $autoload->load($path);
-
 $cache 		= new Cache;
 $path 		= "../dnt-cache/";
-$vendorId 	= "58";
 
 Vendor::getId();
-
 $originDOmain 	= $GLOBALS['ORIGIN_DOMAIN'];
 $dbDomain 		= $GLOBALS['DB_DOMAIN'];
-
 $cache->deleteCacheByDomain($path, $dbDomain);
