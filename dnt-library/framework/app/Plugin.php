6264692967007027597aafe4b6d44a456e927ebe
Tomas Doubek <thomas.doubek@gmail.com>
2019-11-24 18:18:48 +0100

    code clean

--- dnt-library/framework/app/Plugin.php
+++ dnt-library/framework/app/Plugin.php
@@ -1,20 +1,23 @@
 <?php
-class Plugin{
 
-	protected $data;
-	
-	public function __construct($data, $currentPlugin){
-		$this->data = $data;
-		$this->data['ENV'] = Frontend::ENV($data,$currentPlugin);
-	}
-	
-	protected function layout($path,$layout){
-		
-		$data = $this->data;
-		$file = dirname($path) . "/" . $layout . ".php";
-		if(file_exists($file))
-			include $file;
-		else
-			die("layout " . $layout . " not exists");
-	}
+class Plugin {
+
+    protected $data;
+
+    public function __construct($data, $currentPlugin) {
+        $this->data = $data;
+        $this->data['ENV'] = Frontend::ENV($data, $currentPlugin);
+    }
+
+    protected function layout($path, $layout) {
+
+        $data = $this->data;
+        $file = dirname($path) . "/" . $layout . ".php";
+        if (file_exists($file)) {
+            include $file;
+        } else {
+            die("layout " . $layout . " not exists");
+        }
+    }
+
 }
--- dnt-view/layouts/default/modules/default/plugins/content/contentPluginControll.php
+++ dnt-view/layouts/default/modules/default/plugins/content/contentPluginControll.php
@@ -1,10 +1,10 @@
 <?php
 
-class contentPluginControll extends Plugin{
-	
-	public function run(){
-		$data = $this->data;
-		$this->layout(__FILE__ , 'tpl');
-	}
-	
+class contentPluginControll extends Plugin {
+
+    public function run() {
+        $data = $this->data;
+        $this->layout(__FILE__, 'tpl');
+    }
+
 }
