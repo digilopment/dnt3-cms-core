a0a7fe777bc56e9764eeaeb73bb2b0d7f44a6c2f
Tomas Doubek <thomas.doubek@gmail.com>
2020-01-19 18:42:00 +0100

    fix sql bad column

--- dnt-admin/modules/invoices/sql/dnt_basket.sql
+++ dnt-admin/modules/invoices/sql/dnt_basket.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `dnt_basket` (
   `vendor_id` int(11) NOT NULL,
   `user_id` varchar(300) NOT NULL,
   `product_id` int(11) NOT NULL,
-  `order_id` varchar(300) NOT NULL,
+  `order_id_entity` varchar(300) NOT NULL,
   `count` int(11) NOT NULL,
   `datetime_creat` datetime NOT NULL,
   `parent_id` int(11) NOT NULL DEFAULT '0'
