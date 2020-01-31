c568b65259341f4dfde713298bcfba0cc2d939a9
Tomas Doubek <thomas.doubek@gmail.com>
2020-01-31 16:34:26 +0100

    news analytics

---
+++ dnt-api/AnalyticsNewsletters.php
@@ -0,0 +1,46 @@
+<?php
+
+class AnalyticsNewslettersApi extends DntLog
+{
+
+    protected $rest;
+    protected $availableStatus = [
+        'newsletter_log_click',
+        'newsletter_log_delivered',
+    ];
+
+    public function __construct()
+    {
+        $this->rest = new Rest();
+        $this->dnt = new Dnt();
+    }
+
+    public function run()
+    {
+        $systemStatus = $this->rest->get('systemStatus');
+
+        if (in_array($systemStatus, $this->availableStatus)) {
+            $data = [
+                'campaignId' => $this->rest->get('campaignId'),
+                'clickedId' => $this->rest->get('clickedId'),
+                'redirectTo' => $this->rest->get('redirectTo'),
+                'systemStatus' => $systemStatus,
+            ];
+
+            $arr = [
+                'force_log' => true,
+                'msg' => json_encode($data),
+                'system_status' => $data['systemStatus'],
+                'status' => http_response_code(),
+            ];
+
+            $this->add($arr);
+            
+            $redirect = urldecode($this->rest->get('url'));
+            if ($systemStatus == 'newsletter_log_click') {
+                $this->dnt->redirect($redirect);
+            }
+        }
+    }
+
+}
