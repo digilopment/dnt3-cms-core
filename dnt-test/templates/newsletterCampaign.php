0746c9ed45acd1b21ca88cfeb3878ce15358e246
Tomas Doubek <thomas.doubek@gmail.com>
2020-03-02 10:20:05 +0100

    zmena title

--- dnt-test/templates/newsletterCampaign.php
+++ dnt-test/templates/newsletterCampaign.php
@@ -3,7 +3,7 @@
     <head>
         <meta http-equiv="content-type" content="text/html; charset=UTF-8">
         <meta charset="utf-8">
-        <title>Otázky a odpovede | TV Markíza</title>
+        <title>Marketingové kampane | TV Markíza</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <link href="<?php echo $data['baseUrl'] ?>media/bootstrap.min.css" rel="stylesheet">
         <link href="<?php echo $data['baseUrl'] ?>media/jquery.dataTables.min.css" rel="stylesheet">
