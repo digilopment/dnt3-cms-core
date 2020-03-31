4ff6776e29b8de0e2d639f5219efd0afb30b8336
Tomas Doubek <thomas.doubek@gmail.com>
2020-03-31 20:06:07 +0200

    covid find first covid box

--- dnt-api/Covid.php
+++ dnt-api/Covid.php
@@ -5,17 +5,32 @@ class CovidApi
 
     protected $serviceUrl = 'https://ezdravie.nczisk.sk/webapi/v1/kpi';
     protected $json;
+    protected $dnt;
+    protected $firstCovidCase = 0;
 
     public function __construct()
     {
         $this->getData();
+        $this->findFirstCovidBox();
     }
 
     protected function getData()
     {
-
+        $this->dnt = new Dnt();
         $content = file_get_contents($this->serviceUrl);
         $this->json = json_decode($content, true);
+    }
+
+    protected function findFirstCovidBox()
+    {
+        $i = 0;
+        foreach ($this->json['tiles'] as $single) {
+            if ($this->dnt->in_string('COVID-19', $single['name'])) {
+                $this->firstCovidCase = $i;
+                return;
+            }
+            $i++;
+        }
     }
 
     public function covidData()
@@ -23,10 +38,9 @@ class CovidApi
         $final = [];
         $i = 0;
         foreach ($this->json['tiles'] as $single) {
-
             $lastDay = count($single['data']['d']) - 1;
             switch ($i) {
-                case 0:
+                case $this->firstCovidCase:
                     $final['infected'] = [
                         'name' => $single['name'],
                         'updated' => $single['updated'],
@@ -37,7 +51,7 @@ class CovidApi
                         'new' => $single['data']['d'][$lastDay]['v'] - $single['data']['d'][$lastDay - 1]['v'],
                     ];
                     break;
-                case 1:
+                case $this->firstCovidCase + 1:
                     $final['tested'] = [
                         'name' => $single['name'],
                         'updated' => $single['updated'],
@@ -48,7 +62,7 @@ class CovidApi
                         'new' => $single['data']['d'][$lastDay]['v'] - $single['data']['d'][$lastDay - 1]['v'],
                     ];
                     break;
-                case 2:
+                case $this->firstCovidCase + 2:
                     $final['recovered'] = [
                         'name' => $single['name'],
                         'updated' => $single['updated'],
@@ -56,10 +70,10 @@ class CovidApi
                         'customName' => 'Počet vyliečených pacientov',
                         'latest' => $single['data']['d'][$lastDay]['v'],
                         'previous' => $single['data']['d'][$lastDay - 1]['v'],
-                        'new' => $single['data']['d'][$lastDay - 1]['v'] - $single['data']['d'][$lastDay]['v']
+                        'new' => $single['data']['d'][$lastDay]['v'] - $single['data']['d'][$lastDay - 1]['v']
                     ];
                     break;
-                case 3:
+                case $this->firstCovidCase + 3:
                     $final['died'] = [
                         'name' => $single['name'],
                         'updated' => $single['updated'],
