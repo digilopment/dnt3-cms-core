9618a972376d57c316b055e599f5c704a9bcdf18
Tomas Doubek <thomas.doubek@gmail.com>
2020-04-06 23:03:02 +0200

    covid extend local data

--- dnt-api/Covid.php
+++ dnt-api/Covid.php
@@ -4,14 +4,68 @@ class CovidApi
 {
 
     protected $serviceUrl = 'https://ezdravie.nczisk.sk/webapi/v1/kpi';
+
+    const LOCAL_SERVICE_URL = 'https://www.tvnoviny.sk/clanok/1994138_korona-vidget';
+    const TABLE = 'covid-slovensko';
+
     protected $json;
     protected $dnt;
     protected $firstCovidCase = 0;
+    protected $dom;
+    protected $localContent = null;
+    protected $hasLocalData = null;
 
     public function __construct()
     {
+        $this->dom = new DOMDocument();
+        $this->dnt = new Dnt();
+    }
+
+    protected function init()
+    {
+        $this->loadLocalData();
         $this->getData();
         $this->findFirstCovidBox();
+    }
+
+    protected function clean($string)
+    {
+        return str_replace('-', '', $this->dnt->name_url($string));
+    }
+
+    protected function getTable($tableId, $data)
+    {
+        $tempArr = explode(self::TABLE, $data);
+        if (!isset($tempArr[1])) {
+            return null;
+        }
+        $parsed = explode('</table>', $tempArr[1]);
+        $content = '<table id="' . $tableId . $parsed[0] . '</table>';
+        return $content;
+    }
+
+    protected function getHttpResponseCode($url)
+    {
+        $headers = get_headers($url);
+        return substr($headers[0], 9, 3);
+    }
+
+    protected function loadLocalData()
+    {
+        $this->localContent = file_get_contents(self::LOCAL_SERVICE_URL . '?time=' . time());
+        if (isset(explode(self::TABLE, $this->localContent)[1])) {
+            $this->hasLocalData = true;
+        }
+    }
+
+    protected function getValue($elementId, $clean = true)
+    {
+        $content = $this->getTable(self::TABLE, $this->localContent);
+        @$this->dom->loadHTML($content);
+        if ($clean) {
+            return $this->clean($this->dom->getElementById($elementId)->textContent);
+        }
+        return $this->dom->getElementById($elementId)->textContent;
     }
 
     protected function getData()
@@ -31,6 +85,34 @@ class CovidApi
             }
             $i++;
         }
+    }
+
+    protected function covidDataLocal()
+    {
+        $updated = $this->getValue('covid-updated-date', false) . ', ' . $this->getValue('covid-updated-time', false);
+
+        $final['tested'] = [
+            'updatedFormated' => $updated,
+            'latest' => $this->getValue('covid-test-today'),
+            'new' => $this->getValue('covid-test-new'),
+        ];
+        $final['infected'] = [
+            'updatedFormated' => $updated,
+            'latest' => $this->getValue('covid-infected-today'),
+            'new' => $this->getValue('covid-infected-new'),
+        ];
+
+        $final['recovered'] = [
+            'updatedFormated' => $updated,
+            'latest' => $this->getValue('covid-recovered-today'),
+            'new' => $this->getValue('covid-recovered-new'),
+        ];
+        $final['died'] = [
+            'updatedFormated' => $updated,
+            'latest' => $this->getValue('covid-died-today'),
+            'new' => $this->getValue('covid-died-new'),
+        ];
+        return $final;
     }
 
     public function covidData()
@@ -92,7 +174,12 @@ class CovidApi
 
     public function run()
     {
-        print json_encode($this->covidData());
+        $this->init();
+        if ($this->hasLocalData) {
+            print json_encode($this->covidDataLocal());
+        } else {
+            print json_encode($this->covidData());
+        }
     }
 
 }
