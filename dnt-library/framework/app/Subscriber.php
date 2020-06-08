f4117b0bc9f69480e9984b531eb00fd2ec785cc7
Tomas Doubek <thomas.doubek@gmail.com>
2020-06-08 14:43:52 +0200

    fix url

--- dnt-library/framework/app/Subscriber.php
+++ dnt-library/framework/app/Subscriber.php
@@ -27,7 +27,7 @@ class Subscriber
 
     public function generateUrl($id_entity, $email, $status, $domain = false)
     {
-        $finalDomain = ($domain) ? $domain : WWW_PATH . 'subscriber/?params=1';
+        $finalDomain = ($domain) ? $domain.'?plugin=subscriber' : WWW_PATH . 'subscriber/?plugin=subscriber';
         $url = '&id=' . $this->dnt->strToHex(urlencode(base64_encode($email))) . '&vendorId=' . $this->dnt->strToHex(urlencode(base64_encode(Vendor::getId()))) . '&status=' . $this->dnt->strToHex(urlencode(base64_encode($status))) . '&idEntity=' . $this->dnt->strToHex(urlencode(base64_encode($id_entity)));
         return $finalDomain . $url;
     }
