6ca4d3ade280a9560ef89e46f11d658a8e3b661f
Tomas Doubek <thomas.doubek@gmail.com>
2020-08-05 11:34:55 +0200

    update liberty and mayo price

--- dnt-jobs/UpdatePostMeta.php
+++ dnt-jobs/UpdatePostMeta.php
@@ -19,7 +19,7 @@ class UpdatePostMetaJob
     protected function set()
     {
         $this->metaParam = "price";
-        $this->metaValue = "value = 'MAYO'";
+        $this->metaValue = "value = 'MAYO' or value = 'LIBERTY'";
     }
 
     protected function createMOCfromVOC($voc)
@@ -36,12 +36,14 @@ class UpdatePostMetaJob
         $this->set();
         $query = "SELECT * FROM dnt_posts_meta WHERE vendor_id = '" . self::VENDOR_ID . "' AND `key` = '" . $this->metaParam . "' AND post_id IN(SELECT post_id FROM dnt_posts_meta WHERE " . $this->metaValue . ") GROUP BY post_id";
         if ($this->db->num_rows($query) > 0) {
+            $i = 1;
             foreach ($this->db->get_results($query) as $row) {
                 $param = $row['value'];
                 $paramUpdated = $this->createMOCfromVOC($param);
                 $updateQuery = "UPDATE `dnt_posts_meta` SET value='" . $paramUpdated . "' WHERE `key` = '" . $this->metaParam . "' AND id_entity = '" . $row['id_entity'] . "' AND vendor_id = '" . self::VENDOR_ID . "'";
                 $this->db->query($updateQuery);
-                echo $param . " - " . $paramUpdated . "<br/>";
+                echo $i . ' - ' . $param . " - " . $paramUpdated . "<br/>";
+                $i++;
             }
         }
     }
