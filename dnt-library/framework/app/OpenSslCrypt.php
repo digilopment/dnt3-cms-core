0adb2f7ed9efd2e3315b81305eb3bbe384b98444
Tomas Doubek <thomas.doubek@gmail.com>
2020-08-26 12:37:41 +0200

    code clean

--- dnt-library/framework/app/OpenSslCrypt.php
+++ dnt-library/framework/app/OpenSslCrypt.php
@@ -5,10 +5,10 @@ namespace App;
 class OpenSslCrypt
 {
 
-    private $privateKey = 'Dnt3Platform';
-    private $encryptionIv = '1234567891011121';  // Non-NULL Initialization Vector for encryption 
-    private $options = 0;
-    private $cipheringAlg = 'AES-128-CTR'; //
+    private $privateKey = 'Dnt3Platform'; //default key
+    private $encryptionIv = '1234567891011121';  //Non-NULL Initialization Vector for encryption 
+    private $options = 0; //custom options, default = 0
+    private $cipheringAlg = 'AES-128-CTR'; //ciphering Algorithmus
 
     public function __construct($customPrivateKey = false)
     {
@@ -19,7 +19,7 @@ class OpenSslCrypt
 
     public function encrypt($string)
     {
-        $encryption = openssl_encrypt($string, $this->cipheringAlg,$this->privateKey, $this->options, $this->encryptionIv);
+        $encryption = openssl_encrypt($string, $this->cipheringAlg, $this->privateKey, $this->options, $this->encryptionIv);
         return $encryption;
     }
 
