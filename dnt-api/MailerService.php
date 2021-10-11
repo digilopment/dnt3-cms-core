696371f9e8537dd63e6bece5771a4f29fdfd2ae6
Tomas Doubek <thomas.doubek@gmail.com>
2021-10-11 10:26:22 +0200

    class curl + mailer servicwe

---
+++ dnt-api/MailerService.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace DntApi;
+
+use DntLibrary\Base\Mailer;
+use DntLibrary\Base\Rest;
+
+class MailerServiceApi
+{
+
+    public function __construct()
+    {
+        $this->mailer = new Mailer();
+        $this->rest = new Rest();
+    }
+
+    public function run()
+    {
+        #DEFAULT
+        $config['senderEmail'] = $this->rest->post('senderEmail');
+        $config['senderName'] = $this->rest->post('senderName');
+        $config['recipientEmail'] = $this->rest->post('recipientEmail');
+        $config['recipientName'] = $this->rest->post('recipientName');
+        $config['message'] = $this->rest->post('message');
+        $config['subject'] = $this->rest->post('subject');
+        #METHOD
+        $config['method'] = $this->rest->post('method');
+
+        #ATTACHAMENT use stringAttachment or attachment
+        if ($this->rest->post('attachment')) {
+            $config['attachment'] = $this->rest->post('attachment');
+        }
+        if ($this->rest->post('stringAttachment')) {
+            $config['stringAttachment'] = $this->rest->post('stringAttachment');
+        }
+
+        #SENGRID
+        if ($this->rest->post('method') == 'SendGridV2' || $this->rest->post('method') == 'SendGridV3') {
+            $config['send_grid_api_key'] = $this->rest->post('send_grid_api_key');
+            $config['send_grid_api_template_id'] = $this->rest->post('send_grid_api_template_id');
+        }
+
+        #SMTP
+        if ($this->rest->post('method') == 'Smtp') {
+            $config['host'] = $this->rest->post('host');
+            $config['username'] = $this->rest->post('username');
+            $config['password'] = $this->rest->post('password');
+        }
+
+        if ($this->rest->post('senderEmail') && $this->rest->post('recipientEmail') && $this->rest->post('message') && $this->rest->post('subject') && $this->rest->post('method')) {
+            $this->mailer->sent($config);
+            $response = [
+                'success' => 1,
+                'data' => $config,
+            ];
+        } else {
+            $response = [
+                'success' => 0,
+                'data' => $config,
+            ];
+        }
+        print(json_encode($response));
+    }
+
+}
---
+++ dnt-library/framework/app/Curl.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace DntLibrary\App;
+
+class Curl
+{
+
+    private $url;
+    private $options;
+
+    public function __construct($url, array $options = [])
+    {
+        $this->url = $url;
+        $this->options = $options;
+    }
+
+    public function post(array $post)
+    {
+        $ch = curl_init($this->url);
+
+        foreach ($this->options as $key => $val) {
+            curl_setopt($ch, $key, $val);
+        }
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+
+        $response = curl_exec($ch);
+        $error = curl_error($ch);
+        $errno = curl_errno($ch);
+
+        if (is_resource($ch)) {
+            curl_close($ch);
+        }
+
+        if (0 !== $errno) {
+            throw new RuntimeException($error, $errno);
+        }
+
+        return $response;
+    }
+
+}
