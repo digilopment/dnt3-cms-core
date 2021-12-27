ff3282ebe5827e70347a1efe411a4536461ca3e9
Tomas Doubek <thomas.doubek@gmail.com>
2021-12-27 12:36:07 +0100

    performance

---
+++ dnt-test/Performance.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace DntTest;
+
+class PerformanceTest
+{
+
+    public function run()
+    {
+
+        $RUNS = 100000000;
+        $string = str_repeat('a', 1000);
+        $maxChars = 500;
+
+        $start = microtime(true);
+        for ($i = 0; $i < $RUNS; ++$i) {
+            strlen($string) <= $maxChars;
+        }
+
+        for ($i = 0; $i < $RUNS; ++$i) {
+            !isset($string[$maxChars]);
+        }
+        $end = microtime(true);
+        echo ($end - $start) . " s";
+
+
+        phpinfo();
+    }
+
+}
