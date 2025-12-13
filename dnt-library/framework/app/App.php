454e00994b0c83343c8ff3fa38bfe2003e3b4df7
Tomáš Doubek <thomas.doubek@gmail.com>
2025-12-14 00:01:04 +0100

    fix redirect domain

--- dnt-library/framework/app/App.php
+++ dnt-library/framework/app/App.php
@@ -39,10 +39,11 @@ class App
 
     public function run()
     {
+        //var_dump($this->vendor->getColumn('show_real_url'));
         $this->client->setDomain(
             $this->client->realUrl,
             $this->client->wwwPath,
-            $this->client->getSetting('still_redirect_to_domain'),
+            $this->vendor->getColumn('show_real_url'),
             $this->client->getSetting('language')
         );
         $this->post->init();
