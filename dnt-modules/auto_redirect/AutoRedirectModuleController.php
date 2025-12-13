e2d94310ddfd2bbcfa694c544387124e2caddfb7
Tomáš Doubek <thomas.doubek@gmail.com>
2025-12-13 10:18:19 +0100

    fix autoredirect

--- dnt-modules/auto_redirect/AutoRedirectModuleController.php
+++ dnt-modules/auto_redirect/AutoRedirectModuleController.php
@@ -11,6 +11,12 @@ use DntLibrary\Base\Webhook;
 
 class AutoRedirectModuleController extends Client
 {
+    protected $articleList;
+    protected $articleView;
+    protected $rest;
+    protected $webhook;
+    protected $dnt;
+
     public function __construct()
     {
         parent::__construct();
